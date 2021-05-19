ec02dd5f70329f6ec891064d4107da197b40aead
fruit-in <42092501+fruit-in@users.noreply.github.com>
2021-05-19 08:28:25 +0800

    v0.2: + support of LeetCode CN

--- leetcode_hide_locked_problem.user.js
+++ leetcode_hide_locked_problem.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         LeetCode hide locked problem
 // @namespace    https://github.com/fruit-in/leetcode-hide-locked-problem
-// @version      0.1
+// @version      0.2
 // @description  hide locked problems on LeetCode
 // @author       fruit-in
 // @license      MIT License
 // @match        https://leetcode.com/problemset/*/*
+// @match        https://leetcode-cn.com/problemset/*/*
 // @icon         https://assets.leetcode.com/static_assets/public/icons/favicon-192x192.png
 // @grant        none
 // ==/UserScript==
@@ -13,9 +14,17 @@
 window.onload = function() {
     'use strict';
     let problemset = document.querySelector('.reactable-data');
-    for (var i = problemset.childElementCount - 1; i >= 0; i--) {
-        if (problemset.children[i].children[2].children[0].children[1]) {
-            problemset.children[i].remove();
+    if (location.host == "leetcode.com") {
+        for (let i = problemset.childElementCount - 1; i >= 0; i--) {
+            if (problemset.children[i].children[2].children[0].children[1]) {
+                problemset.children[i].remove();
+            }
+        }
+    } else if (location.host == "leetcode-cn.com") {
+        for (let i = problemset.childElementCount - 1; i >= 0; i--) {
+            if (problemset.children[i].children[0].children[0].children[0]) {
+                problemset.children[i].remove();
+            }
         }
     }
 };
